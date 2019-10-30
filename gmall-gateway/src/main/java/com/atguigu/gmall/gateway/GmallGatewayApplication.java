5f6d9b7b3123da09aced2f9147af78f1901e1a83
glhGitHub274135882 <274135882@qq.com>
2019-10-30 08:42:39 +0800

    gateway项目搭建

---
+++ gmall-gateway/src/main/java/com/atguigu/gmall/gateway/GmallGatewayApplication.java
@@ -0,0 +1,16 @@
+package com.atguigu.gmall.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+
+@SpringBootApplication
+
+public class GmallGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GmallGatewayApplication.class, args);
+    }
+
+}
