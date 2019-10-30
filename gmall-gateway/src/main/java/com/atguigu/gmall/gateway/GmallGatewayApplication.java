52799dcba63dd158ac1f4d068bd0656c98d1b15d
glhGitHub274135882 <274135882@qq.com>
2019-10-30 18:20:41 +0800

    gateway问题提交

--- gmall-gateway/src/main/java/com/atguigu/gmall/gateway/GmallGatewayApplication.java
+++ gmall-gateway/src/main/java/com/atguigu/gmall/gateway/GmallGatewayApplication.java
@@ -3,10 +3,9 @@ package com.atguigu.gmall.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @SpringBootApplication
-
+@EnableDiscoveryClient
 public class GmallGatewayApplication {
 
     public static void main(String[] args) {
